166eb7de715dfc39fe1886e0a9de6bec1324ec02
Jared <jared@playgen.com>
2016-10-12 12:11:39 +0100

    gamework restructure

---
+++ UnityProject/Assets/Editor/BuildGameWorkPackage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public static class BuildGameWorkPackage
+{
+    [MenuItem("Tools/Build GameWork Package")]
+    public static void Build()
+    {
+        var rootDir = Directory.GetParent(Application.dataPath).Parent.FullName;
+        var packageFile = rootDir + "/Build/GameWork.Unity.unitypackage";
+        var gameWorkDir = "Assets/GameWork";
+
+        var packageAssetPaths = new List<string>();
+
+        foreach (var assetPath in AssetDatabase.GetAllAssetPaths())
+        {
+            if (assetPath.StartsWith(gameWorkDir) 
+                && File.Exists(assetPath)   // is file?
+                && !Path.GetDirectoryName(assetPath).Contains(".Tests"))
+            {
+                
+                packageAssetPaths.Add(assetPath);
+            }
+        }
+
+        var packageDir = Path.GetDirectoryName(packageFile);
+        if (!Directory.Exists(packageDir))
+        {
+            Directory.CreateDirectory(packageDir);
+        }
+
+        AssetDatabase.ExportPackage(packageAssetPaths.ToArray(), packageFile);
+
+        Debug.Log("Exported package to: \"" + packageFile  + "\"");
+    }
+}
