717f9e3672f921b7438a4fae4f92db5cf1ee61c7
Jared <jaredgglass@gmail.com>
2017-01-15 19:31:44 +0000

    core update, switch scene state transitions

---
+++ GameWork.Unity.Engine.States/ChangeSceneEventStateTransition.cs
@@ -0,0 +1,32 @@
+﻿using GameWork.Core.States.Event;
+using UnityEngine.SceneManagement;
+
+namespace GameWork.Unity.Engine.States
+{
+	public class ChangeSceneEventStateTransition : EventStateTransition
+	{
+		private readonly string _toSceneName;
+		private readonly string _toStateName;
+
+		public ChangeSceneEventStateTransition(string toStateName, string toSceneName)
+		{
+			_toStateName = toStateName;
+			_toSceneName = toSceneName;
+		}
+
+		public void ChangeState()
+		{
+			ExitState(_toStateName);
+
+			SceneManager.sceneLoaded += OnSceneLoaded;
+			SceneManager.LoadScene(_toSceneName);
+		}
+
+		private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+		{
+			SceneManager.sceneLoaded -= OnSceneLoaded;
+
+			EnterState(_toStateName);
+		}
+	}
+}
---
+++ GameWork.Unity.Engine.States/ChangeSceneTickStateTransition.cs
@@ -0,0 +1,9 @@
+﻿namespace GameWork.Unity.Engine.States
+{
+	public abstract class ChangeSceneTickStateTransition : ChangeSceneEventStateTransition
+	{
+		protected ChangeSceneTickStateTransition(string toStateName, string toSceneName) : base(toStateName, toSceneName)
+		{
+		}
+	}
+}
